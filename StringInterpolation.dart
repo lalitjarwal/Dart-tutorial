ada9d5fa0eabb282a50b9d4a8d81d65006ce2bd4
Lalit Jarwal <64301870+lalitjarwal@users.noreply.github.com>
2020-04-27 13:42:08 +0530

    Create StringInterpolation.dart

---
+++ StringInterpolation.dart
@@ -0,0 +1,22 @@
+void main(){
+  
+  //ways to define Strings in Dart
+  
+  String s1='single';
+  String s2="double";
+  String s3='It\'s easy.';
+  String s4="It's easy.";
+  
+  String s5='This is a very long string.'+
+    ' It can cover the entire screen of the system.';
+  
+  //string Interpolation 
+  
+  print("String s5 is $s5");
+  
+  int length=5;
+  int breadth=6;
+  
+  print("Area of ractangle is ${length*breadth}.");
+
+}
