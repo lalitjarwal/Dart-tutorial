6b19715b18dde503850ac010c239f32d1a062182
Lalit Jarwal <64301870+lalitjarwal@users.noreply.github.com>
2020-04-28 11:32:37 +0530

    Demo of final and const keyword

---
+++ finalAndconstDemo.dart
@@ -0,0 +1,14 @@
+void main(){
+  //final keyword
+  final name="Peter";
+  final String fullName='Peter Parker';
+  
+  //const keyword
+  const PI=3.14;
+  const double gravity=9.8;
+  
+}
+class Demo{
+  final color='Red';
+  static const PI=3.14; //only const PI=3.14 gives an error
+}
