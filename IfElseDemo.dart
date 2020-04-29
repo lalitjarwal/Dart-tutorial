dc41501bc3a5f19e8583cf9f88731a3e92008ce8
Lalit Jarwal <64301870+lalitjarwal@users.noreply.github.com>
2020-04-29 17:21:19 +0530

    if else Statement Demo

---
+++ IfElseDemo.dart
@@ -0,0 +1,18 @@
+void main(){
+  
+  //IF ELSE Ladder Statement Or Nested If Else
+  
+  var myNum=10;
+  var num2=7;
+  var num3=20;
+  
+  if(myNum>num2 && myNum>num3){
+    print("Maximum number is $myNum");
+  }
+  else if(num2>myNum&& num2>num3){
+    print("Maximum number is $myNum");
+  }
+  else{
+    print("Maximum number is $num3");
+  }
+}
