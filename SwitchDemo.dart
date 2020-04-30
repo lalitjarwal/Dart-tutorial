6c45a67bd9926ba169c5563e2090932b2a5d6549
Lalit Jarwal <64301870+lalitjarwal@users.noreply.github.com>
2020-04-30 12:14:32 +0530

    Switch demo with break statement

---
+++ SwitchDemo.dart
@@ -0,0 +1,31 @@
+void main(){
+  var day=4;
+  switch(day){
+    case 1:
+      print("Monday");
+      break;
+    case 2:
+      print("Tuesday");
+      break;
+    case 3:
+      print("Wednesday");
+      break;
+    case 4:
+      print("Thursday");
+      break;
+    case 5:
+      print("Friday");
+      break;
+    case 6:
+      print("Saturday");
+      break;
+    case 7:
+      print("Sunday");
+      break;
+    default:
+      print("Invalid");
+      break;
+      
+      
+  }
+}
