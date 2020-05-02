4874cc6a862a9ca4e4885633e948a90be30f52a3
Lalit Jarwal <64301870+lalitjarwal@users.noreply.github.com>
2020-05-02 11:53:39 +0530

    For loop demo

---
+++ ForLoopDemo.dart
@@ -0,0 +1,12 @@
+void main(){
+  
+  //For loop demo
+  for(int i=1;i<=10;i+=2)
+    print(i);
+  
+  //List of items containing same data type
+  List myList=['Apple','Banana','Mango','Orange'];
+  
+  for(String fruits in myList ) //For ... in  loop Demo
+    print(fruits);
+}
