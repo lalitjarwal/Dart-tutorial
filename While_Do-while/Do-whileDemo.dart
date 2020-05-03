9539889643892628764911dea046b7a1b8a5171f
Lalit Jarwal <64301870+lalitjarwal@users.noreply.github.com>
2020-05-03 13:12:10 +0530

    do-while loop Demo

---
+++ While_Do-while/Do-whileDemo.dart
@@ -0,0 +1,15 @@
+void main(){
+  
+  //Initializing counter
+  var counter=1;
+  
+ do
+  {
+    print("value is ${counter*2}"); 
+    
+    /* ${exp} used for functional call in String
+    interpollation*/
+    
+    counter++;  //increment counter
+  }while(counter<6);
+}
