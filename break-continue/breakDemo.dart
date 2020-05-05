87ee3ac65a175ebd88cbb8d8277ce6e59325d797
Lalit Jarwal <64301870+lalitjarwal@users.noreply.github.com>
2020-05-05 13:45:06 +0530

    break Statement

---
+++ break-continue/breakDemo.dart
@@ -0,0 +1,11 @@
+void main() {
+  //Labelled for loop
+  outerLoop:for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 3; j+=2) {
+      print("$i $j");
+      if (i !=2 && j!=2) {
+        break outerLoop; //break using label
+      }
+    }
+  }
+}
